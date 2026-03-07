a3834bd9fae7ab835855d004b6bbfc6ec31b8387
Ultralisho”git config --global user.email “alumuliseshernandez@gmail.com <alumuliseshernandez@gmail.com>
2026-03-07 10:58:12 +0100

    fix leaderboard routes

--- brawlstats-backend/routes/leaderboard.routes.js
+++ brawlstats-backend/routes/leaderboard.routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const ctrl   = require('../controllers/leaderboard.controller');
 const { protect } = require('../middlewares/auth.middleware');
 
-router.get('/global',        protect, ctrl.global);
-router.get('/country/:code', protect, ctrl.byCountry);
+router.get('/global',        ctrl.global);
+router.get('/country/:code', ctrl.byCountry);
 router.get('/local',         protect, ctrl.local);
 
 module.exports = router;
